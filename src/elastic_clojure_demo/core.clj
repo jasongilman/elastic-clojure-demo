52232663ba4af1e1f0c5720558c3859b4db6f26b
Jason Gilman <jason@element84.com>
2014-04-26 10:38:23 -0400

    Initial commit

---
+++ src/elastic_clojure_demo/core.clj
@@ -0,0 +1,6 @@
+(ns elastic-clojure-demo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/elastic_clojure_demo/core_test.clj
@@ -0,0 +1,7 @@
+(ns elastic-clojure-demo.core-test
+  (:require [clojure.test :refer :all]
+            [elastic-clojure-demo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
